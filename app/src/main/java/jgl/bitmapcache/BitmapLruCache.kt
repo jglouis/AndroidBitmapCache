99f2c981667eab5f3195d734aae1544b4545e38b
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2018-01-11 16:16:31 +0100

    Refactor getBytesPerPixel

--- app/src/main/java/jgl/bitmapcache/BitmapLruCache.kt
+++ app/src/main/java/jgl/bitmapcache/BitmapLruCache.kt
@@ -167,14 +167,11 @@ fun scale(bitmap: Bitmap, scalingFactor: Float): Bitmap {
 }
 
 fun getBytesPerPixel(config: Bitmap.Config): Int {
-    if (config === Bitmap.Config.ARGB_8888) {
-        return 4
-    } else if (config === Bitmap.Config.RGB_565) {
-        return 2
-    } else if (config === Bitmap.Config.ARGB_4444) {
-        return 2
-    } else if (config === Bitmap.Config.ALPHA_8) {
-        return 1
+    return when(config) {
+        Bitmap.Config.ARGB_8888 -> 4
+        Bitmap.Config.RGB_565 -> 2
+        Bitmap.Config.ARGB_4444 -> 2
+        Bitmap.Config.ALPHA_8 -> 1
+        else -> 1
     }
-    return 1
 }
